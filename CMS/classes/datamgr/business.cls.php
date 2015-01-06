2514ed1d540fa5517e9994864ccd2d7d02fc7e72
Steve Cai <steve.cai@morningstar.com>
2015-01-06 17:22:41 +0800

    fix bug

--- CMS/classes/datamgr/business.cls.php
+++ CMS/classes/datamgr/business.cls.php
@@ -27,22 +27,13 @@
 	{
 		$sum=0;
 		
-		$sql="select count(1) from dr_tb_member_vaccine_order where status='C' and h_status='P' ";
-		$query = $this->dbmgr->query($sql);
-		$result = $this->dbmgr->fetch_array($query); 
-		$sum=$sum+ $result[0];
+		//$sql="select count(1) from dr_tb_member_vaccine_order where status='C' and h_status='P' ";
+		//$query = $this->dbmgr->query($sql);
+		//$result = $this->dbmgr->fetch_array($query); 
+		//$sum=$sum+ $result[0];
 
 		
-		$sql="select count(1) from dr_tb_requirement where handling_user=$user_id and handling_type='P'  ";
-		$query = $this->dbmgr->query($sql);
-		$result = $this->dbmgr->fetch_array($query); 
-		$sum=$sum+ $result[0];
-
 		
-		$sql="select count(1) from dr_tb_requirement where submit_user=$user_id and handling_type<>'P'  and review_type='P'  ";
-		$query = $this->dbmgr->query($sql);
-		$result = $this->dbmgr->fetch_array($query); 
-		$sum=$sum+ $result[0];
 		return $sum;
 	}
 
@@ -50,66 +41,26 @@
 		Global $CONFIG;
 		$Array=Array();
 
-		$arr=Array();
-		$arr["name"]="疫苗预约取消";
-		$user_id=mysql_real_escape_string($user_id);
-		$sql="select  m.id,m.name as first,
- m.mobile  as second,m.order_date  as third 
-from dr_tb_member_vaccine_order m
-		where status='C' and h_status='P'
-        order by m.updated_date desc
-		limit 0,3 ";
-		$query = $this->dbmgr->query($sql);
-		$result = $this->dbmgr->fetch_array_all($query); 
-		$arr["result"]=$result;
-		$arr["first"]="客户姓名";
-		$arr["second"]="手机号码";
-		$arr["third"]="预约日期";
-		$arr["count"]=count($result);
-		$arr["link"]=$CONFIG['rootpath']."/Appointment/appiontment.php";
-		$Array[]=$arr;
+		//$arr=Array();
+		//$arr["name"]="疫苗预约取消";
+		//$user_id=mysql_real_escape_string($user_id);
+		//$sql="select  m.id,m.name as first,
+ //m.mobile  as second,m.order_date  as third 
+//from dr_tb_member_vaccine_order m
+		//where status='C' and h_status='P'
+        //order by m.updated_date desc
+		//limit 0,3 ";
+		//$query = $this->dbmgr->query($sql);
+		//$result = $this->dbmgr->fetch_array_all($query); 
+		//$arr["result"]=$result;
+		//$arr["first"]="客户姓名";
+		//$arr["second"]="手机号码";
+		//$arr["third"]="预约日期";
+		//$arr["count"]=count($result);
+		//$arr["link"]=$CONFIG['rootpath']."/Appointment/appiontment.php";
+		//$Array[]=$arr;
 
 		
-		$arr=Array();
-		$arr["name"]="需求等待修改";
-		$user_id=mysql_real_escape_string($user_id);
-		$sql="select  m.id,m.title as first,
-case m.type when 'R' then '新需求' else  'Bug' end as second,user.user_name as third 
-from dr_tb_requirement m
-inner join dr_tb_user user on m.submit_user=user.id
-		where handling_user=$user_id and handling_type='P' 
-        order by m.updated_date desc
-		limit 0,3 ";
-		$query = $this->dbmgr->query($sql);
-		$result = $this->dbmgr->fetch_array_all($query); 
-		$arr["result"]=$result;
-		$arr["first"]="需求标题";
-		$arr["second"]="需求类型";
-		$arr["third"]="提交人";
-		$arr["count"]=count($result);
-		$arr["link"]=$CONFIG['rootpath']."/Other/requirement.php";
-		$Array[]=$arr;
-
-		
-		$arr=Array();
-		$arr["name"]="需求等待审阅";
-		$user_id=mysql_real_escape_string($user_id);
-		$sql="select  m.id,m.title as first,
-case m.handling_type when 'F' then '已处理' else  '不处理' end as second,user.user_name as third 
-from dr_tb_requirement m
-inner join dr_tb_user user on m.handling_user=user.id
-		where submit_user=$user_id and handling_type<>'P'  and review_type='P'
-        order by m.updated_date desc
-		limit 0,3 ";
-		$query = $this->dbmgr->query($sql);
-		$result = $this->dbmgr->fetch_array_all($query); 
-		$arr["result"]=$result;
-		$arr["first"]="需求标题";
-		$arr["second"]="处理结果";
-		$arr["third"]="处理人";
-		$arr["count"]=count($result);
-		$arr["link"]=$CONFIG['rootpath']."/Other/requirement.php";
-		$Array[]=$arr;
 
 		return $Array;
 
