d882757aa36244f6436c6f573eabe2cb31480557
Steve Cai <steve.cai@morningstar.com>
2015-08-18 18:13:01 +0800

    完成数据库自动生成工具

--- AsisstantTools/DatabaseCreatorForm.cs
+++ AsisstantTools/DatabaseCreatorForm.cs
@@ -1,5 +1,5 @@
-﻿using AsisstantTools.MyControls;
-using MySql.Data.MySqlClient;
+﻿using MySql.Data.MySqlClient;
+using SunStar.DataBaseFactory;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -18,6 +18,8 @@ namespace AsisstantTools
 {
     public partial class DatabaseCreatorForm : Form
     {
+        Dictionary<string, string> matchValueType = new Dictionary<string, string>();
+
         public DatabaseCreatorForm()
         {
             InitializeComponent();
@@ -28,6 +30,19 @@ namespace AsisstantTools
             cmbDatabaseProvider.Items.Add("MSSQL");
             cmbDatabaseProvider.Items.Add("MYSQL");
             cmbDatabaseProvider.SelectedItem = cmbDatabaseProvider.Items[0];
+
+            matchValueType.Add("select", "varchar(10)");
+            matchValueType.Add("text", "nvarchar(100)");
+            matchValueType.Add("longtext", "nvarchar(500)");
+            matchValueType.Add("datetime", "datetime");
+            matchValueType.Add("html", "text");
+            matchValueType.Add("upload", "nvarchar(255)");
+            matchValueType.Add("flist", "nvarchar(500)");
+            matchValueType.Add("number", "decimal(10,2)");
+            matchValueType.Add("check", "varchar(1)");
+            matchValueType.Add("fkey", "int");
+            matchValueType.Add("grid", "");
+
         }
 
 
@@ -36,17 +51,17 @@ namespace AsisstantTools
             
         }
 
-        DbConnection GetDBConn()
+        DBInstance GetDBInstance()
         {
-            DbConnection db=null;
+            DBInstance db = null;
             if (cmbDatabaseProvider.Text == "MSSQL")
             {
-                db = new SqlConnection(txtConnectionStr.Text);
+                db = new DBInstance(SqlClientFactory.Instance, txtConnectionStr.Text);
                 //databaseServerConnectionControl.TestConnect();
             }
             else if (cmbDatabaseProvider.Text == "MYSQL")
             {
-                db = new MySqlConnection(txtConnectionStr.Text);
+                db = new DBInstance(MySqlClientFactory.Instance, txtConnectionStr.Text);
                 //databaseServerConnectionControl.TestConnect();
             }
             return db;
@@ -56,9 +71,10 @@ namespace AsisstantTools
         {
             try
             {
-                using (DbConnection db = GetDBConn())
+                DBInstance dn = GetDBInstance();
+                using (DbConnection conn = dn.GetDbConnection())
                 {
-                    db.Open();
+                    conn.Open();
                     MessageBox.Show("Database Connected Successfully");
                 }
             }
@@ -82,22 +98,139 @@ namespace AsisstantTools
                     {
                         XmlDocument doc = new XmlDocument();
                         doc.Load(xmlfile);
-                        using (DbConnection db = GetDBConn())
+                        DBInstance dn = GetDBInstance();
+                        using (DbConnection conn = dn.GetDbConnection())
                         {
+                            conn.Open();
+                            XmlNode root = doc.SelectSingleNode("root");
+                            string tablename = root.SelectSingleNode("tablename").InnerText;
+                            string ismutillanguage = root.SelectSingleNode("ismutillang") == null ? "" : root.SelectSingleNode("ismutillang").InnerText;
+                            CheckAndCreateTable(dn, tablename, ismutillanguage == "1");
 
+                            XmlNode fields = root.SelectSingleNode("fields");
+                            XmlNodeList fieldlist = fields.SelectNodes("field");
+                            foreach (XmlNode field in fields)
+                            {
+                                string fieldname = field.SelectSingleNode("key").InnerText;
+                                string fieldtype = field.SelectSingleNode("type").InnerText;
+                                string ismutillangfield = field.SelectSingleNode("ismutillang") == null ? "" : root.SelectSingleNode("ismutillang").InnerText;
+                                CreateField(dn, tablename, fieldname, fieldtype, ismutillangfield == "1" && ismutillanguage == "1");
+                            }
                         }
                     }
                     catch (Exception ex)
                     {
-                        txtConsole.SelectionColor=Color.Red;
-                        txtConsole.AppendText(string.Format("[Xml File Read Fail]{0}:{1}", xmlfile, ex.Message));
-                        txtConsole.SelectionColor = Color.Black;
+                        string msg = string.Format("[Xml File Read Fail]{0}:{1}", xmlfile, ex.Message);
+                        AddToConsole(msg,true);
                     }
                 }
             }
         }
 
-        
+        private void CreateField(DBInstance dn, string tablename, string fieldname, string fieldtype, bool ismulti)
+        {
+            if (!matchValueType.ContainsKey(fieldtype))
+            {
+                string msg = string.Format("[Cannot find field type]{0}", fieldtype);
+                AddToConsole(msg, true);
+                return;
+            }
 
+            string fieldrealtype = matchValueType[fieldtype];
+            if (fieldrealtype == "")
+            {
+                return;
+            }
+            if (ismulti)
+            {
+                tablename = tablename + "_l";
+            }
+            string sql="";
+            try
+            {
+                dn.ExecuteNonQuery(string.Format("select {0} from {1}", fieldname,tablename), null);
+                sql = string.Format(@"ALTER TABLE {0}
+ALTER COLUMN {1} {2}", tablename, fieldname, fieldrealtype);
+                AddToConsole(string.Format("[Column modify Success]{0}:{1}:{2}:{3}", tablename,fieldname,fieldtype,fieldrealtype,sql));
+            }
+            catch
+            {
+                try
+                {
+                    sql = string.Format(@"ALTER TABLE {0}
+ADD  {1} {2}", tablename, fieldname, fieldrealtype);
+                    dn.ExecuteNonQuery(sql, null);
+                    AddToConsole(string.Format("[Column add Success]{0}:{1}:{2}:{3}", tablename, fieldname, fieldtype, fieldrealtype, sql));
+                }
+                catch (Exception ex)
+                {
+                    string msg = string.Format("[Column add failed]{0}:{1}:{2}:{3}:{4}", tablename, fieldname, fieldtype, fieldrealtype,ex.Message,sql);
+                    AddToConsole(msg, true);
+                }
+            }
+        }
+
+        private void CheckAndCreateTable(DBInstance dn, string tablename, bool ismutillang)
+        {
+            string sql = string.Format(@"
+CREATE TABLE {0}
+(
+id int primary key,
+created_date datetime,
+created_user int,
+updated_date datetime,
+updated_user int
+)
+", tablename);
+            CreateTable(dn, tablename, sql);
+            if (ismutillang)
+            {
+                tablename = tablename + "_l"; 
+                sql = string.Format(@"
+CREATE TABLE {0}
+(
+oid int primary key,
+lang varchar(10) primary key
+)
+", tablename);
+                CreateTable(dn, tablename,sql);
+            }
+
+
+        }
+
+        private void CreateTable(DBInstance dn, string tablename,string sql)
+        {
+            try
+            {
+                dn.ExecuteNonQuery(string.Format("select id from {0}", tablename), null);
+                AddToConsole(string.Format("[Table exists]{0}", tablename));
+            }
+            catch
+            {
+                try
+                {
+
+                    
+                    dn.ExecuteNonQuery(sql, null);
+                    AddToConsole(string.Format("[Table created Success]{0}", tablename));
+                }
+                catch (Exception ex)
+                {
+                    string msg = string.Format("[Table created Fail]{0}:{1}", tablename, ex.Message);
+                    AddToConsole(msg, true);
+                }
+            }
+        }
+
+        private void AddToConsole(string msg, bool isAlert = false)
+        {
+            if (isAlert)
+            {
+                txtConsole.SelectionColor = Color.Red;
+            }
+            txtConsole.AppendText(msg+"\r\n");
+            txtConsole.SelectionColor = Color.Black;
+        }
     }
 }
